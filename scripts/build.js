1fd5cf4e356856766ee06fa96c9bfa46a5ca9db0
Anil <anil@FVFWL2GMHV27.local>
2019-01-21 13:58:15 +0530

    fix

--- scripts/build.js
+++ scripts/build.js
@@ -10,7 +10,7 @@ process.env.NODE_ENV = 'production';
 process.on('unhandledRejection', err => {
   throw err;
 });
-//require('dotenv').config();
+require('dotenv').config();
 // Ensure environment variables are read.
 require('../config/env');
 
